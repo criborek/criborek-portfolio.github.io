7bc16e7b6393bd0779ec8e6a647096e086b2c5dd
criborek <159389787+criborek@users.noreply.github.com>
2025-06-29 20:15:09 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,10 @@
 import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel/serverless";
+import { defineConfig } from 'astro/config'
 
-// If you are not going to use vercel analytics you can delete the analytics
-// If you want to remove vercel from your project you can do npm uninstall vercel
 export default defineConfig({
-  site: 'https://criborek-portfolio.github.io',
-  base: 'criborek',
-  output: "server",
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true,
-    },
-  }),
+  site: 'https://astronaut.github.io',
+  base: 'my-repo',
 });
 
 //If you don't have vercel the configuration will be empty
